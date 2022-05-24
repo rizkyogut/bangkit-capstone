aac7cbead49896cdbf99abd6cdd46b9750d4eb56
rizkyogut <mrizky.maul99@gmail.com>
2022-05-24 14:23:16 +0700

    test 1

---
+++ app/src/main/java/com/bangkit2022/spicedetectobject/MainActivity.kt
@@ -0,0 +1,32 @@
+package com.bangkit2022.spicedetectobject
+
+import android.os.Bundle
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.bangkit2022.spicedetectobject.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val navView: BottomNavigationView = binding.navView
+
+        val navController = findNavController(R.id.nav_host_fragment_activity_main)
+        // Passing each menu ID as a set of Ids because each
+        // menu should be considered as top level destinations.
+        val appBarConfiguration = AppBarConfiguration(setOf(
+            R.id.navigation_home, R.id.navigation_camera, R.id.navigation_article))
+        setupActionBarWithNavController(navController, appBarConfiguration)
+        navView.setupWithNavController(navController)
+    }
+}
---
+++ app/src/main/java/com/bangkit2022/spicedetectobject/ui/article/ArticleViewModel.kt
@@ -0,0 +1,7 @@
+package com.bangkit2022.spicedetectobject.ui.article
+
+import androidx.lifecycle.ViewModel
+
+class ArticleViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
