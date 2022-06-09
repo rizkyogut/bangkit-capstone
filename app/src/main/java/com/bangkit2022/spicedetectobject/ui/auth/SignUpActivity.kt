355886ba72a9a9ddcd0cd5fd2f9b288069163c94
rizkyogut <mrizky.maul99@gmail.com>
2022-06-09 10:30:38 +0700

    Add SingIn and SingUp Layout

---
+++ app/src/main/java/com/bangkit2022/spicedetectobject/ui/auth/SignUpActivity.kt
@@ -0,0 +1,12 @@
+package com.bangkit2022.spicedetectobject.ui.auth
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.bangkit2022.spicedetectobject.R
+
+class SignUpActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sign_up)
+    }
+}
